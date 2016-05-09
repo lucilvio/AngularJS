147eb74532a9ad5fe153d9c51449676dadae1d09
Lucilvio Alves de Lima <Lucilvio Alves de Lima>
2016-05-09 00:10:41 -0300

    First version

---
+++ index.js
@@ -0,0 +1,23 @@
+angular.module("sample", []).controller("todoController", function() {
+   
+    this.tasks = [
+        {
+            titulo: "Tarefa 1"
+        },
+        {
+            titulo: "Tarefa 2"
+        }
+    ]
+    
+    this.adicionarTarefa = function() {
+        this.tasks.push({ titulo: this.newTask })
+        this.newTask = '';
+    }
+    
+    this.marcarConcluida = function() {
+        if(this.concluida) {
+            this.classConcluida = "concluida";
+        }
+    }
+    
+});
